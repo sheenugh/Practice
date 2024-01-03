1f0f91d0a6cedcc4d28f3bc59e91a2f96e7fbe9b
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-03 17:18:02 +0800

    Asking the user how much oranges he/she wants to buy

--- practice_assignment3_part2.py
+++ practice_assignment3_part2.py
@@ -1,6 +1,12 @@
 #pseudocode
 
-# - Ask user for the total number of apple he/she wants to buy
-asking_the_total_number_of_apple_a_user_wants_to_buy = int(input("Please input a number: "))
+# - Ask user for the total number of apples he/she wants to buy
+asking_the_total_number_of_apples_a_user_wants_to_buy = int(input("Please input a number: "))
+print("User wants to buy: " + str(asking_the_total_number_of_apples_a_user_wants_to_buy) + " apples")
 
-print("User wants to buy: " + str(asking_the_total_number_of_apple_a_user_wants_to_buy))
+print("\n")
+
+# - Ask user user for the total number of oranges he/she wants to buy
+asking_the_total_number_of_oranges_a_user_wants_to_buy = int(input("Please input a number: "))
+print("User wants to buy: " + str(asking_the_total_number_of_oranges_a_user_wants_to_buy) + " oranges")
+
