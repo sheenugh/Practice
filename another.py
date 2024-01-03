5ab3dadd1a25b5da855a3639a070998b4a5de5f7
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-03 21:02:38 +0800

    Malapit na matapos

--- another.py
+++ another.py
@@ -9,6 +9,9 @@
 def asking_the_user(dessert_type):
     return int(input("Please Enter the Total Number of " + dessert_type + " you want to buy: "))
 
+def computation_of_the_money(type_of_dessert, dessert_price):
+    return type_of_dessert * dessert_price
+
 # ========= IMPORT =========
 
 
@@ -21,6 +24,10 @@ yogurt_dessert = asking_the_user("yogurt")
 # - Price of the Desserts
 ice_cream_price = 150
 yogurt_price = 100
+
+# - Multiplying each of the dessert from the user's desired total she/he picked by its constant price
+product_of_ice_cream_dessert = ice_cream_dessert * ice_cream_price
+
 
 
 # Note: Use a "define" function when executing this task
