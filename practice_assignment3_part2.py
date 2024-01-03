15052999f0530c52dd2fdebcd7a5a6ffff005277
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-03 17:22:57 +0800

    Asking the user how much oranges he/she wants to buy

--- practice_assignment3_part2.py
+++ practice_assignment3_part2.py
@@ -2,11 +2,16 @@
 
 # - Ask user for the total number of apples he/she wants to buy
 asking_the_total_number_of_apples_a_user_wants_to_buy = int(input("Please input a number: "))
-print("User wants to buy: " + str(asking_the_total_number_of_apples_a_user_wants_to_buy) + " apples")
+print("The user wants to buy: " + str(asking_the_total_number_of_apples_a_user_wants_to_buy) + " apples")
 
 print("\n")
 
 # - Ask user user for the total number of oranges he/she wants to buy
 asking_the_total_number_of_oranges_a_user_wants_to_buy = int(input("Please input a number: "))
-print("User wants to buy: " + str(asking_the_total_number_of_oranges_a_user_wants_to_buy) + " oranges")
+print("The user wants to buy: " + str(asking_the_total_number_of_oranges_a_user_wants_to_buy) + " oranges")
+
+# - Assigning price for apple and orange
+apple = 20
+oranges = 40
+
 
