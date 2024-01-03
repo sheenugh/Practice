04b4ee714e4ee3269639c83655f5e0f818ed7e43
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-03 16:53:24 +0800

    pseudocode

--- practice_assignment3_part2.py
+++ practice_assignment3_part2.py
@@ -1,2 +1,6 @@
 #pseudocode
 
+# - Ask user for the total number of apple he/she wants to buy
+asking_the_total_number_of_apple_a_user_wants_to_buy = int(input("Please input a number: "))
+
+print("User wants to buy: " + str(asking_the_total_number_of_apple_a_user_wants_to_buy))
