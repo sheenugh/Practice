b0b4ca1d786183e2f92f1d303c2a6d3160be74ea
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-03 20:43:54 +0800

    The Beginning

---
+++ another.py
@@ -0,0 +1,19 @@
+#Delima, Sheena Mae D.
+#BSCPE1-5
+
+
+#-----------------------------------------------
+
+
+# ========= FUNCTIONS =========
+
+
+# ========= IMPORT =========
+
+
+# ========= ACTUAL CODES =========
+# -- Pseudocode --
+# Note: Use a "define" function when executing this task
+# Total amount of change
+# Price of the Desserts
+
