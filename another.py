859190bd5c5b2c3c59a0153ee0ca981b91163b57
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-03 20:53:51 +0800

    Added def function in asking the user on how many he/she will buy

--- another.py
+++ another.py
@@ -6,14 +6,26 @@
 
 
 # ========= FUNCTIONS =========
-
+def asking_the_user(dessert_type):
+    return int(input("Please Enter the Total Number of " + dessert_type + " you want to buy: "))
 
 # ========= IMPORT =========
 
 
 # ========= ACTUAL CODES =========
-# -- Pseudocode --
+# --- Pseudocode --
+# - Variable for the dessert type and for the def function + print function
+ice_cream_dessert = asking_the_user("ice cream")
+yogurt_dessert = asking_the_user("yogurt")
+
+# - Price of the Desserts
+ice_cream_price = 150
+yogurt_price = 100
+
+
 # Note: Use a "define" function when executing this task
 # Total amount of change
-# Price of the Desserts
+
+
+
 
